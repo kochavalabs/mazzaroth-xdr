f277bc3976b3c4a646860c7a40e2d06c8dc1d365
Kyle Bartush <kbartush@kochava.com>
2019-04-22 08:13:34 -0700

    Write two files with 1000 txs to compare file size.

--- examples/write_file.go
+++ examples/write_file.go
@@ -1,22 +1,45 @@
 package main
 
 import (
-	"fmt"
+	"github.com/golang/protobuf/proto"
 	"github.com/kochavalabs/mazzaroth-proto/pb"
 	"github.com/kochavalabs/mazzaroth-xdr/xdr"
+	"os"
 )
 
-// Write a transaction XDR file with 100 TXs and an equicalent proto file for
-// comparison
+const (
+	protoFile = "pbout"
+	xdrFile   = "xdrout"
+	txWritten = 10000
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+// Write a transaction XDR file with txWritten TXs and an equicalent proto file
+// for comparison
 func main() {
 	signature := [64]byte{1, 2, 3}
 	address := [32]byte{6, 7, 8}
-	// contract := []byte{1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+	contract := []byte{1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 	channelID := [32]byte{3, 4, 5}
+
 	txXdr := xdr.Transaction{
 		Signature: signature,
 		Address:   address,
-		Action:    xdr.Action{},
+		Action: xdr.Action{
+			ChannelId: channelID,
+			Nonce:     4,
+			Category: xdr.ActionCategory{
+				Type: xdr.ActionCategoryTypeUpdate,
+				Update: &xdr.Update{
+					Contract: contract,
+				},
+			},
+		},
 	}
 
 	txPb := pb.Transaction{
@@ -25,9 +48,26 @@ func main() {
 		Action: &pb.Action{
 			ChannelId: channelID[:],
 			Nonce:     4,
+			ActionCategory: &pb.Action_Update{
+				Update: &pb.Update{
+					Contract: contract,
+				},
+			},
 		},
 	}
 
-	fmt.Println(txXdr)
-	fmt.Println(txPb)
+	pF, err := os.Create(protoFile)
+	check(err)
+	defer pF.Close()
+
+	xF, err := os.Create(xdrFile)
+	check(err)
+	defer xF.Close()
+
+	for i := 0; i < txWritten; i++ {
+		pbBytes, _ := proto.Marshal(&txPb)
+		pF.Write(pbBytes)
+
+		xdr.Marshal(xF, txXdr)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,7 +1,7 @@
 module github.com/kochavalabs/mazzaroth-xdr
 
 require (
-	github.com/golang/protobuf v1.3.1 // indirect
+	github.com/golang/protobuf v1.3.1
 	github.com/grpc-ecosystem/grpc-gateway v1.8.5 // indirect
 	github.com/kochavalabs/mazzaroth-proto v0.0.0-20190416200046-75dba5a24799
 	github.com/stellar/go-xdr v0.0.0-20180917104419-0bc96f33a18e
