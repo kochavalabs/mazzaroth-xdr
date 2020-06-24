89fc142e14b0f3c03638801d9873195416fe6059
Kyle Bartush <kbartush@kochava.com>
2020-06-24 16:23:08 -0700

    feat(string): if arguments are strings, the result should be a string too

--- xdr/xdr_generated.go
+++ xdr/xdr_generated.go
@@ -1209,7 +1209,7 @@ type Receipt struct {
 
 	Result string `json:"result"`
 
-	StatusInfo StatusInfo `json:"status_info"`
+	Result string
 }
 
 // MarshalBinary implements encoding.BinaryMarshaler.
