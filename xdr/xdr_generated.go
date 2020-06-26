02ae8a48099ac803d93a7ced864eec6a859f7d46
Jeff Simpson <jeff1010322@gmail.com>
2020-06-26 13:27:26 -0400

    fix(rpc): updated result in ReadonlyResponse to be string

--- xdr/xdr_generated.go
+++ xdr/xdr_generated.go
@@ -1562,7 +1562,7 @@ var (
 
 // ReadonlyResponse generated struct
 type ReadonlyResponse struct {
-	Result string `json:"result"`
+	Result string
 
 	StateStatus StateStatus `json:"state_status"`
 
