9cbd0b904417b74bd04b244617c38bb4bba6124c
Giulio Ministeri <gministeri@kochava.com>
2021-04-08 14:02:02 +0200

    TOBESQUASHED - fixing comments

--- xdr/common.go
+++ xdr/common.go
@@ -53,12 +53,12 @@ func IDFromPublicKey(pk crypto.PublicKey) (ID, error) {
 	return IDFromSlice(bbytes)
 }
 
-// MarshalJSON : when marshaling to JSON we want ID to be represented as a base64-encoded string, not as an array of uint8
+// MarshalJSON : when marshaling to JSON we want ID to be represented as a hex-encoded string, not as an array of uint8
 func (id ID) MarshalJSON() ([]byte, error) {
 	return json.Marshal(hex.EncodeToString(id[:]))
 }
 
-// UnmarshalJSON : when marshaling to JSON we want ID to be represented as a base64-encoded string, not as an array of uint8
+// UnmarshalJSON : when marshaling to JSON we want ID to be represented as a hex-encoded string, not as an array of uint8
 func (id *ID) UnmarshalJSON(b []byte) error {
 	var s string
 	if err := json.Unmarshal(b, &s); err != nil {
@@ -75,12 +75,12 @@ func (id *ID) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
-// MarshalJSON : when marshaling to JSON we want ID to be represented as a base64-encoded string, not as an array of uint8
+// MarshalJSON : when marshaling to JSON we want signature to be represented as a hex-encoded string, not as an array of uint8
 func (sig Signature) MarshalJSON() ([]byte, error) {
 	return json.Marshal(hex.EncodeToString(sig[:]))
 }
 
-// UnmarshalJSON : when marshaling to JSON we want ID to be represented as a base64-encoded string, not as an array of uint8
+// UnmarshalJSON : when marshaling to JSON we want Signature to be represented as a hex-encoded string, not as an array of uint8
 func (sig *Signature) UnmarshalJSON(b []byte) error {
 	var s string
 	if err := json.Unmarshal(b, &s); err != nil {
@@ -97,12 +97,12 @@ func (sig *Signature) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
-// MarshalJSON : when marshaling to JSON we want ID to be represented as a base64-encoded string, not as an array of uint8
+// MarshalJSON : when marshaling to JSON we want Hash to be represented as a hex-encoded string, not as an array of uint8
 func (h Hash) MarshalJSON() ([]byte, error) {
 	return json.Marshal(hex.EncodeToString(h[:]))
 }
 
-// UnmarshalJSON : when marshaling to JSON we want ID to be represented as a base64-encoded string, not as an array of uint8
+// UnmarshalJSON : when marshaling to JSON we want Hash to be represented as a hex-encoded string, not as an array of uint8
 func (h *Hash) UnmarshalJSON(b []byte) error {
 	var s string
 	if err := json.Unmarshal(b, &s); err != nil {
@@ -119,12 +119,12 @@ func (h *Hash) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
-// MarshalJSON : when marshaling to JSON we want ID to be represented as a base64-encoded string, not as an array of uint8
+// MarshalJSON : when marshaling to JSON we want Hash32 to be represented as a hex-encoded string, not as an array of uint8
 func (h Hash32) MarshalJSON() ([]byte, error) {
 	return json.Marshal(hex.EncodeToString(h[:]))
 }
 
-// UnmarshalJSON : when marshaling to JSON we want ID to be represented as a base64-encoded string, not as an array of uint8
+// UnmarshalJSON : when marshaling to JSON we want Hash32 to be represented as a hex-encoded string, not as an array of uint8
 func (h *Hash32) UnmarshalJSON(b []byte) error {
 	var s string
 	if err := json.Unmarshal(b, &s); err != nil {
@@ -141,12 +141,12 @@ func (h *Hash32) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
-// MarshalJSON : when marshaling to JSON we want ID to be represented as a base64-encoded string, not as an array of uint8
+// MarshalJSON : when marshaling to JSON we want Hash64 to be represented as a hex-encoded string, not as an array of uint8
 func (h Hash64) MarshalJSON() ([]byte, error) {
 	return json.Marshal(hex.EncodeToString(h[:]))
 }
 
-// UnmarshalJSON : when marshaling to JSON we want ID to be represented as a base64-encoded string, not as an array of uint8
+// UnmarshalJSON : when marshaling to JSON we want Hash64 to be represented as a hex-encoded string, not as an array of uint8
 func (h *Hash64) UnmarshalJSON(b []byte) error {
 	var s string
 	if err := json.Unmarshal(b, &s); err != nil {
