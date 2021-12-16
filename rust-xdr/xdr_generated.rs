587bff1aa8136bd5d169ff370d55db6080f43a65
Jeff Simpson <jeff1010322@gmail.com>
2021-12-16 16:24:50 -0500

    fix(rust): include macro option for generated rust code

--- rust-xdr/xdr_generated.rs
+++ rust-xdr/xdr_generated.rs
@@ -1,6 +1,7 @@
 #![allow(non_snake_case)]
 #![allow(non_camel_case_types)]
-
+#[macro_use]
+extern crate xdr_rs_serialize_derive;
 #[allow(unused_imports)]
 use std::io::Write;
 #[allow(unused_imports)]
